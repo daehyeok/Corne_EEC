86bb8256ed2fa249ce2a51a8b44764397d9eae62
Daehyeok Mun <daehyeok@google.com>
2025-09-05 11:19:06 -0600

    production

--- hardware/kicad_asset/scripts/flip.py
+++ hardware/kicad_asset/scripts/flip.py
@@ -2,6 +2,44 @@
 import pcbnew
 import os
 
+
+def renumber():
+    # board = pcbnew.GetBoard()
+    for fp in board.GetFootprints():
+        ref = fp.GetReference()
+        prefix = ''
+        num= 0
+        if not ref.startswith('D')  and not ref.startswith('MX'):
+            continue
+
+        if ref.startswith('D'):
+            if ref[1].isalpha() and ref[1] != '-1':
+                continue
+            else:
+                num = int(ref[1:])
+            prefix='D'
+        else:
+            num = int(ref[2:])
+            prefix='MX'
+
+        if num <7:
+            continue
+
+        row = (num - 22) // 6 + 1
+        base = 6 * row + 1
+        col =  5 - ( (num - 22  )   %  6 )
+        num = base + col
+        print(f"{ref} -> {prefix}{num}")
+
+        res.append((ref, f"base {base} - col {col} : {num}"))
+        #      27 26 25 24 23 22
+        #      5  4  3  2  1  0
+        # base 7
+        # col  0 1 2  3  4   5
+        # fp.SetReference(f"{prefix}{num} - base {base} - col {col}")
+
+
+renumber()
 def flip(pcb_board, new_board_path=""):
     zero = pcbnew.VECTOR2I(0,0)
 
@@ -10,14 +48,14 @@ def flip(pcb_board, new_board_path=""):
         prev_orientation = fp.GetOrientationDegrees()
         flip_left_right = False
         ref = fp.GetReference().strip()
-            
+
         fp.Flip(prev_position, flip_left_right)
         fp.Move(prev_position - fp.GetPosition())
 
         if prev_orientation != 0:
             fp.SetOrientationDegrees(-prev_orientation)
 
-        fp.SetLocked(True)
+        # fp.SetLocked(True)
 
 
     for track in pcb_board.GetTracks():
@@ -29,6 +67,7 @@ def flip(pcb_board, new_board_path=""):
         track.SetEndY(end.y)
 
     if(new_board_path != ""):
+
         pcbnew.SaveBoard(new_board_path, pcb_board)
 
 # After update Kicad7 on Mac, Run script from cli not wokring well.
